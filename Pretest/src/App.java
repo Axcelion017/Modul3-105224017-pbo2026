24948614c6b18586aff18653bfc95750289cd1b1
Axcelion017 <michaelnewton1708@gmail.com>
2026-03-10 14:06:11 +0700

    Pretest

---
+++ Pretest/src/App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan usia anda: ");
+        int usia = sc.nextInt();
+        System.out.print("Masukan berat badan anda: ");
+        double berat = sc.nextDouble();
+        System.out.print("Masukan kadar hemoglobin anda: ");
+        double hemoglobin = sc.nextDouble();
+        sc.close();
+
+        if (usia < 17) {
+            System.out.println("Maaf, anda belum cukup umur untuk mendonorkan darah");
+        } else if (usia > 60) {
+            System.out.println("Maaf, anda sudah terlalu tua untuk mendonorkan darah");
+        } else {
+            if (berat <= 45) {
+                System.out.println("Maaf, berat badan anda tidak mencukupi untuk mendonorkan darah");
+            } else {
+                if (hemoglobin < 12.5) {
+                    System.out.println("Maaf, kadar hemoglobin anda tidak mencukupi untuk mendonorkan darah");
+                } else if (hemoglobin > 17.0) {
+                    System.out.println("Maaf, kadar hemoglobin anda terlalu tinggi untuk mendonorkan darah");
+                } else {
+                    System.out.println("Anda memenuhi syarat untuk mendonorkan darah");
+                }
+            }
+        }
+
+    }
+}  
