75f3864d67eb5c05ba6057253d36238d477cb9ec
Axcelion017 <michaelnewton1708@gmail.com>
2026-03-10 16:19:45 +0700

    Posttest

--- Posttest/src/App.java
+++ Posttest/src/App.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        int saldo = 500000;
+        int menu = 0;
+        do{
+            System.out.println("Program Mesin ATM");
+            System.out.println("1. Cek Saldo");
+            System.out.println("2. Setor Tunai");
+            System.out.println("3. Tarik Tunai");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih Menu: ");
+            menu = input.nextInt();
+            switch(menu){
+                case 1:
+                    System.out.printf("Saldo Anda: Rp%d \n", saldo);
+                    break;
+                case 2:
+                    System.out.print("Masukkan jumlah setor tunai: Rp");
+                    int setor = input.nextInt();
+                    saldo += setor;
+                    System.out.printf("Saldo Anda setelah setor tunai: Rp%d \n", saldo);
+                    break;
+                case 3:
+                    System.out.println("Masukkan jumlah tarik tunai: Rp");
+                    int tarik = input.nextInt();
+                    if(tarik > saldo){
+                        System.out.println("Saldo tidak cukup untuk melakukan tarik tunai.");
+                    } else {
+                        saldo -= tarik;
+                        System.out.printf("Saldo Anda setelah tarik tunai: Rp%d \n", saldo);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Terima kasih telah menggunakan ATM.");
+                    input.close();
+                    break;
+                default:
+                    System.out.println("Menu tidak valid. Silakan pilih menu yang tersedia.");
+                }
+        }while(menu != 4);
     }
 }
